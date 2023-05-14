4a93426879856fa77eb1d392a811d39cc484d47b
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-14 18:28:04 +0200

    Update variables.js

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://wordpress-local:8888";
+export const BASE_URL = "https://markmystories.com";
 export const POSTS_API_URL = "/wp-json/wp/v2/posts/";
 export const MENU_URL = "/getallitems.php";
